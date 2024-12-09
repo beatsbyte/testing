d137ef8a79204276a2b10df4892e3d4b93af38a5
Shohruh Rahmatillayev <rshohrux16@gmail.com>
2024-12-09 08:33:18 +0000

    some typos

--- conftest.py
+++ conftest.py
@@ -1,72 +1,106 @@
-# conftest.py
 import pytest
 from pydub import AudioSegment
 import numpy as np
 import os
 import aiohttp
-import asyncio
-from aiohttp.payload import BytesPayload
-import requests
+from aiohttp.payload import BytesPayload, StringPayload
+from dotenv import load_dotenv
+import logging
+from pprint import pprint
+
+load_dotenv()
+url = os.getenv("URL")
 @pytest.fixture
 async def send_music():
-    async def _send_music(url, file_path, id):
-        try:
-            with open(file_path, "rb") as file:
-                file_content = file.read()
-        except FileNotFoundError:
-            pytest.fail(f"Test MP3 file not found: {file_path}")
-
+    async def _send_music(file_path: str, level: int):
+        with open(file_path, "rb") as file:
+            file_content = file.read()
 
         with aiohttp.MultipartWriter() as data:
-            payload = BytesPayload(
+            file_payload = BytesPayload(
                 file_content,
                 content_type="audio/mpeg",
-                filename="test_input.mp3"
+                filename=file_path
             )
+            
+            part = data.append_payload(file_payload)
+            part.set_content_disposition('form-data', name='file', filename=file_path)
 
-            part = data.append_payload(payload)
-            part.set_content_disposition('form-data', name='file', filename="test_input.mp3")
+            level_payload = StringPayload(
+                str(level),
+                content_type="text/plain"
+            )
+            part = data.append_payload(level_payload)
+            part.set_content_disposition('form-data', name='compress_degree')
 
         headers = {
             "Content-Type": f"multipart/form-data; boundary={data.boundary}",
         }
 
+        session = aiohttp.ClientSession()
+        response = await session.post(f'{url}/v1/compress', data=data, headers=headers)
+        return response
 
-        async with aiohttp.ClientSession() as session:
-                async with session.post(f'{url}/v1/compress', data=data, headers=headers) as response:
-                    if response.status != 200:
-                        pytest.fail(f"Request failed with status: {response.status}")
-                    ogg_content = await response.read()
-
-        # ogg_content = await response.read()
-        output_file_path = f"data/test_output_{id}.ogg"
-        with open(output_file_path, "wb") as output_file:
-            output_file.write(ogg_content)
-        
-        return output_file_path
-    
     return _send_music
-
 
 @pytest.fixture
 def audio2npArray():
     def _audio2npArray(audio_path):
-        audio = AudioSegment.from_file(audio_path)
-        audio_common_type = "data/audio.wav"
-        audio.export(audio_common_type, format="wav")
-        audio = AudioSegment.from_wav(audio_common_type).set_channels(1)
+        audio = AudioSegment.from_mp3(audio_path).set_channels(1)
         return np.array(audio.get_array_of_samples())
     return _audio2npArray
 
 @pytest.fixture
-def compare_audio():
-    def _compare_audio(np_array1, np_array2):
+def similarity():
+    def _similarity(np_array1, np_array2):
         min_len = min(len(np_array1), len(np_array2))
         np_array1 = np_array1[:min_len]
         np_array2 = np_array2[:min_len]
         
         correlation = np.corrcoef(np_array1, np_array2)[0, 1]
-        correlation = max(correlation, 0)
-        return correlation * 100
+        return abs(correlation) * 100
 
-    return _compare_audio
+    return _similarity
+
+
+@pytest.fixture
+def getsize():
+    def _getsize(file_path):
+        return os.path.getsize(file_path)//1024
+    return _getsize
+
+@pytest.fixture
+async def send_music_list(send_music, getsize):
+    async def _send_music_list(file_paths, level):
+        total_size = 0
+        total_compressed_size = 0
+        for file in os.listdir(file_paths):
+            file_path = f"{file_paths}/{file}"
+            response = await send_music(file_path, level)
+
+            logging.info(f"Used backend url: {response.headers['X-Proxy-Worker-Url']}")
+            response_path = f"responses/{file.split('.')[0]}_response_{level}.mp3"
+            with open(response_path, "wb") as file:
+                file.write(await response.read())
+            
+            logging.info(f"Compressed file saved at {response_path}.")
+            logging.info(f"Compress result: {getsize(file_path)} KB -> {getsize(response_path)} KB")
+            pprint(f"Compress result: {file_path} :: {getsize(file_path)} KB -> {getsize(response_path)} KB")
+
+            total_size += getsize(file_path)
+            total_compressed_size += getsize(response_path)
+
+        return total_size, total_compressed_size
+    return _send_music_list
+
+
+@pytest.fixture
+async def calc_similarity(audio2npArray, similarity):
+    async def _calc_similarity(file_path, response_path):
+        file_data = audio2npArray(file_path)
+        response_data = audio2npArray(response_path)
+
+        similarity_percentage = similarity(file_data, response_data)
+        similarity_percentage = round(similarity_percentage, 2)
+        return similarity_percentage
+    return _calc_similarity
--- test_basic.py
+++ test_basic.py
@@ -2,34 +2,70 @@ import pytest
 from aiohttp.payload import BytesPayload
 import aiohttp
 import os
+import logging
+from dotenv import load_dotenv
 
-def number_sequence():
-    count = 1
-    while True:
-        yield str(count)
-        count += 1
+load_dotenv()
+
+url = os.getenv("URL")
+
+async def test_send_music_easy(send_music, send_music_list):
+    response = await send_music_list("temp", 0)
+    total_size, total_compressed_size = response
+    print(f"easy: {total_size} KB -> {total_compressed_size} KB")
+
+async def test_send_music_medium(send_music, send_music_list):
+    response = await send_music_list("temp", 1)
+    total_size, total_compressed_size = response
+    print(f"medium: {total_size} KB -> {total_compressed_size} KB")
+
+async def test_send_music_hard(send_music, send_music_list):
+    response = await send_music_list("temp", 2)
+    total_size, total_compressed_size = response
+    print(f"hard: {total_size} KB -> {total_compressed_size} KB")
+
+
+async def test_similarity_easy(send_music, calc_similarity):
+    test_path = "music/music1.mp3"
+    response_path = "responses/music1_response_easy.mp3"
+    response = await send_music(test_path, 0)
+
+    with open(response_path, "wb") as file:
+        file.write(await response.read())
+
+    similarity_percentage = await calc_similarity(test_path, response_path)
+    print("Easy: ", similarity_percentage)
+    assert similarity_percentage >= 85
+
+async def test_similarity_medium(send_music, calc_similarity):
+    test_path = "music/music1.mp3"
+    response_path = "responses/music1_response_medium.mp3"
+    response = await send_music(test_path, 1)
     
-seq = number_sequence()
+    with open(response_path, "wb") as file:
+        file.write(await response.read())
 
-url = "http://192.168.0.60:8080"
-file_path = "data/test_input.mp3"
-async def test_send_music(send_music):
-    output_file_path = await send_music(url, file_path, next(seq))
-    assert os.path.exists(output_file_path)
+    similarity_percentage = await calc_similarity(test_path, response_path)
+    print("Medium: ", similarity_percentage)
+    assert similarity_percentage >= 70
 
+async def test_similarity_hard(send_music, calc_similarity):
+    test_path = "music/music1.mp3"
+    response_path = "responses/music1_response_hard.mp3"
+    response = await send_music(test_path, 2)
 
-async def test_check_audio(send_music, audio2npArray, compare_audio):
-    output_file_path = await send_music(url, file_path, next(seq))
-    np_array1 = audio2npArray(file_path)
-    np_array2 = audio2npArray(output_file_path)
-    correlation = compare_audio(np_array1, np_array2)
-    assert correlation > 85
+    with open(response_path, "wb") as file:
+        file.write(await response.read())
+
+    similarity_percentage = await calc_similarity(test_path, response_path)
+    print("Hard: ", similarity_percentage)
+    assert similarity_percentage >= 50
 
 async def test_service_alive():
     async with aiohttp.ClientSession() as session:
         async with session.put(f'{url}/v1/imalive', json={'url': 'v1/compress'}) as response:
             assert response.status == 200
+            logging.info("Service is alive")
 
 def test_basic():
     assert 1 == 1
-
